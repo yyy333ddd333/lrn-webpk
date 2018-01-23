6a7a48f5b86e0adeb096eb27d12652b15f195c21
Yang Feng <yyy333ddd333@hotmail.com>
2018-01-24 00:14:14 +0800

    initial

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    devtool: 'eval-source-map',
+    entry: __dirname + "/app/main.js",//已多次提及的唯一入口文件
+    output: {
+        path: __dirname + "/public",//打包后的文件存放的地方
+        filename: "bundle.js"//打包后输出文件的文件名
+    },
+    devServer: {
+        contentBase: './public',
+        historyApiFallback: true,
+        inline: true
+    },
+    module: {
+        rules: [
+            {
+                test: /(\.jsx|\.js)$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            'env', 'react'
+                        ]
+                    }
+                },
+                exclude: /node_modules/
+            }
+        ]
+    }
+}
