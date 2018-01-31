ba769116a6e26757b06d4d1fe0712f13933175a9
Yang Feng <yyy333ddd333@hotmail.com>
2018-01-31 22:33:14 +0800

    学到Hot Module Replacement

--- app/main.js
+++ app/main.js
@@ -2,4 +2,5 @@ import React from 'react';
 import {render} from 'react-dom';
 import Greeter from './Greeter';
 
+import './main.css'
 render(<Greeter />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-    devtool: 'eval-source-map',
     entry: __dirname + "/app/main.js",//已多次提及的唯一入口文件
     output: {
-        path: __dirname + "/public",//打包后的文件存放的地方
+        path: __dirname + "/build",//打包后的文件存放的地方
         filename: "bundle.js"//打包后输出文件的文件名
     },
+    devtool: 'eval-source-map',
     devServer: {
         contentBase: './public',
         historyApiFallback: true,
@@ -18,7 +20,30 @@ module.exports = {
                     loader: 'babel-loader'
                 },
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [{
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            localIdentName: '[name]__[local]--[hash:base64:5]'
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    }
+                ]
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.BannerPlugin('杨枫的财产神圣不可侵犯'),
+        new HtmlWebpackPlugin({
+            tempalte: __dirname + '/app/index.tmpl.html'
+        })
+    ]
 }
